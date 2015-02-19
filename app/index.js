62f750c13df29d0d075792add2d107b156a23b00
Aaron Williams <aaron.colby.williams@gmail.com>
2015-02-19 12:52:41 -0800

    prompt setup implemented

--- app/index.js
+++ app/index.js
@@ -1,7 +1,6 @@
 'use strict';
 var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
-var yosay = require('yosay');
 
 module.exports = yeoman.generators.Base.extend({
   initializing: function () {
@@ -12,19 +11,70 @@ module.exports = yeoman.generators.Base.extend({
     var done = this.async();
 
     // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the stunning' + chalk.red('Newsapp') + ' generator!'
+    this.log(require('yosay')());
+    this.log(chalk.magenta(
+      'Welcome to the stunning ' + chalk.red('Newsapp') + ' generator!' +
+      'Out of the box I include HTML5 Boilerplate, jQuery, and a ' +
+      'Gruntfile.js to build your app.'
     ));
 
+
     var prompts = [{
+      type: 'list',
+      name: 'template',
+      message: 'Choose application template:',
+      choices: [{
+        name: 'Django',
+        value: 'includeDjango'
+      }, {
+        name: 'Webapp',
+        value: 'includeWebapp'
+      }]
+    },{
+      type: 'checkbox',
+      name: 'features',
+      message: 'What more would you like?',
+      choices: [{
+        name: 'Bootstrap',
+        value: 'includeBootstrap',
+        checked: true
+      },{
+        name: 'Sass',
+        value: 'includeSass',
+        checked: false
+      },{
+        name: 'Modernizr',
+        value: 'includeModernizr',
+        checked: false
+      }]
+    }, {
+      when: function (answers) {
+        return answers && answers.features &&
+          answers.features.indexOf('includeSass') !== -1;
+      },
       type: 'confirm',
-      name: 'someOption',
-      message: 'Would you like to enable this option?',
-      default: true
+      name: 'libsass',
+      value: 'includeLibSass',
+      message: 'Would you like to use libsass? Read up more at \n' +
+        chalk.green('https://github.com/andrew/node-sass#node-sass'),
+      default: false
     }];
 
-    this.prompt(prompts, function (props) {
-      this.someOption = props.someOption;
+    this.prompt(prompts, function (answers) {
+      var features = answers.features;
+
+      function hasFeature(feat) {
+        return features && features.indexOf(feat) !== -1;
+      }
+
+      this.includeSass = hasFeature('includeSass');
+      this.includeBootstrap = hasFeature('includeBootstrap');
+      this.includeModernizr = hasFeature('includeModernizr');
+      this.includeDjango =  hasFeature('includeDjango');
+      this.includeWebapp = hasFeature('includeWebapp');
+
+      this.includeLibSass = answers.libsass;
+      this.includeRubySass = !answers.libsass;
 
       done();
     }.bind(this));
